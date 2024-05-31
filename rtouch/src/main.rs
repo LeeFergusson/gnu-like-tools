9ea58be9111dce1613345ebf660ac735adf2bc33
Lee Fergusson <lee.fergusson@googlemail.com>
2024-05-31 10:48:14 +0100

    Restructure project to use clap

--- rtouch/src/main.rs
+++ rtouch/src/main.rs
@@ -1,41 +1,90 @@
-use clap::Arg;
+use clap::Parser;
 use std::{
-  env::args,
   fs::{File, FileTimes, OpenOptions},
+  io::{Error, ErrorKind},
 };
 
 fn main() {
-  args()
-    .skip(1)
-    .for_each(|path| match OpenOptions::new().write(true).open(&path) {
-      Ok(file) => {
-        let time = std::time::SystemTime::now();
-        match file.set_times(FileTimes::new().set_accessed(time).set_modified(time)) {
-          Ok(_) => println!("File updated: {}", path),
-          Err(error) => match error.kind() {
-            std::io::ErrorKind::PermissionDenied => {
-              eprintln!("Error updating file: Permission denied");
-            }
-            _ => {
-              eprintln!("Error updating file: {}", error);
-            }
-          },
-        };
+  let args = Args::parse();
+
+  for file in &args.files {
+    match update_file(file, &args) {
+      Ok(_) => {}
+      Err(error) => {
+        eprintln!("Error updating file: {}", error);
       }
-      Err(error) => match error.kind() {
-        std::io::ErrorKind::NotFound => {
-          match File::create(&path) {
-            Ok(_) => {
-              println!("New file created: {}", path);
-            }
+    }
+  }
+}
+
+// Argument parsing. ----------------------------------------------------------
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+/// Update the access and modification times of each FILE to the current time.
+struct Args {
+  /// Change the access time only.
+  #[arg(short('a'), long = None, conflicts_with = "update_modification_only", default_value = "false")]
+  update_access_only: bool,
+
+  /// Do not create any files.
+  #[arg(short('c'), long("no-create"), default_value = "false")]
+  no_create: bool,
+
+  /// Parse the date string.
+  #[arg(short('d'), long("date"), default_value = None, conflicts_with = "update_access_only")]
+  date: Option<String>,
+
+  /// Change the modification time only.
+  #[arg(short('m'), long = None)]
+  update_modification_only: bool,
+
+  /// Use this file's times instead of the current time.
+  #[arg(short('r'), long("reference"))]
+  file_reference: Option<String>,
+
+  /// Files to update.
+  #[arg(name = "FILES", required = true)]
+  files: Vec<String>,
+}
+
+// Functions. -----------------------------------------------------------------
+
+/// Update the access and modification times of a file.
+fn update_file(file: &str, args: &Args) -> Result<(), Error> {
+  // Attempt to open the file for writing.
+  match OpenOptions::new().write(true).open(file) {
+    Ok(file) => {
+      let time = std::time::SystemTime::now();
+      let file_times = FileTimes::new();
+
+      // Update the file's times depending on the arguments.
+      if args.update_access_only {
+        file.set_times(file_times.set_accessed(time))?;
+      } else if args.update_modification_only {
+        file.set_times(file_times.set_modified(time))?;
+      } else {
+        file.set_times(file_times.set_accessed(time).set_modified(time))?;
+      }
+    }
+    Err(error) => match error.kind() {
+      ErrorKind::PermissionDenied => {
+        eprintln!("Error updating file: Permission denied");
+      }
+      ErrorKind::NotFound => {
+        if !args.no_create {
+          match File::create(file) {
+            Ok(_) => {}
             Err(error) => {
               eprintln!("Error creating file: {}", error);
             }
           };
         }
-        _ => {
-          eprintln!("Error opening file: {}", error);
-        }
-      },
-    });
+      }
+      _ => {
+        eprintln!("Error opening file: {}", error);
+      }
+    },
+  };
+  Ok(())
 }
