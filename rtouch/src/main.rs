4a67875b3a3fed367e8c3144c9c80a71a7093eac
Lee Fergusson <lee.fergusson@googlemail.com>
2024-06-02 18:23:37 +0100

    Move argument parsing to septate function

--- rtouch/src/main.rs
+++ rtouch/src/main.rs
@@ -21,11 +21,11 @@ struct Args {
   no_create: bool,
 
   /// Parse the date string.
-  #[arg(short('d'), long("date"), default_value = None, conflicts_with = "update_access_only")]
+  #[arg(short('d'), long("date"), default_value = None)]
   date: Option<String>,
 
   /// Change the modification time only.
-  #[arg(short('m'), long = None)]
+  #[arg(short('m'), long = None, conflicts_with = "update_access_only", default_value = "false")]
   update_modification_only: bool,
 
   /// Use this file's times instead of the current time.
@@ -42,23 +42,9 @@ fn main() {
   let args = Args::parse();
   let mut file_time = SystemTime::now();
 
-  // If a date was provided, attempt to parse it.
-  if let Some(ref time) = args.date {
-    let parsed_date =
-      chrono::DateTime::parse_from_str(&time, "%Y-%m-%d %H:%M:%S.%3f %z");
-    match parsed_date {
-      Ok(date) => {
-        if let Some(date_time) = DateTime::from_timestamp(0, 0) {
-          let duration = date.signed_duration_since(date_time);
-          file_time = SystemTime::UNIX_EPOCH
-            + Duration::from_secs(duration.num_seconds() as u64);
-        }
-      }
-      Err(_) => {
-        eprintln!(
-          "Invalid date format. Use: -d \"YYYY-MM-DD HH:MM:SS.sss +HHMM\""
-        );
-      }
+  if let Some(time) = &args.date {
+    if let Some(system_time) = parse_time(&time) {
+      file_time = system_time;
     }
   }
 
@@ -74,18 +60,49 @@ fn main() {
 
 // Functions. -----------------------------------------------------------------
 
-/// Update the access and modification times of a file.
+/// ## Parse the time string.
+///
+/// ### Arguments:
+/// * `time` - The time string to parse.
+///
+/// ### Returns:
+/// * `Option<SystemTime>` - The parsed time.
+fn parse_time(time: &str) -> Option<SystemTime> {
+  let parsed_time = DateTime::parse_from_str(time, "%Y-%m-%d %H:%M:%S.%3f %z");
+  match parsed_time {
+    Ok(offset) => {
+      if let Some(date_time) = DateTime::from_timestamp(0, 0) {
+        let duration = offset.signed_duration_since(date_time);
+        return Some(
+          SystemTime::UNIX_EPOCH
+            + Duration::from_secs(duration.num_seconds() as u64),
+        );
+      }
+    }
+    Err(_) => {
+      eprintln!(
+        "Invalid date format. Use: -d \"YYYY-MM-DD HH:MM:SS.sss +HHMM\""
+      );
+    }
+  }
+  None
+}
+
+/// ## Update the access and modification times of a file.
+///
+/// ### Arguments:
+/// * `file` - The file to update.
+/// * `time` - The time to update the file to.
+/// * `args` - The command line arguments.
+///
+/// ### Returns:
+/// * `Result<(), Error>` - The result of the operation.
 fn update_file(file: &str, time: SystemTime, args: &Args) -> Result<(), Error> {
-  // Attempt to open the file for writing.
   match OpenOptions::new().write(true).open(file) {
-    Ok(file) => match get_file_times(time, args) {
-      Ok(file_times) => {
-        file.set_times(file_times)?;
-      }
-      Err(error) => {
-        eprintln!("{}", error);
-      }
-    },
+    Ok(file) => {
+      file.set_times(get_file_times(time, args))?;
+      return Ok(());
+    }
     Err(error) => match error.kind() {
       ErrorKind::PermissionDenied => {
         eprintln!("Error updating file: Permission denied");
@@ -93,10 +110,8 @@ fn update_file(file: &str, time: SystemTime, args: &Args) -> Result<(), Error> {
       ErrorKind::NotFound => {
         if !args.no_create {
           match File::create(file) {
-            Ok(_) => {}
-            Err(error) => {
-              eprintln!("Error creating file: {}", error);
-            }
+            Ok(_) => update_file(file, time, args)?,
+            Err(error) => eprintln!("Error creating file: {}", error),
           };
         }
       }
@@ -108,14 +123,22 @@ fn update_file(file: &str, time: SystemTime, args: &Args) -> Result<(), Error> {
   Ok(())
 }
 
-/// Get the file times to update.
-fn get_file_times(time: SystemTime, args: &Args) -> Result<FileTimes, Error> {
+/// ## Get the file times to update.
+///
+/// ### Arguments:
+/// * `time` - The time to update the file to.
+/// * `args` - The command line arguments.
+///
+/// ### Returns:
+/// * `FileTimes` - The file times to apply.
+fn get_file_times(time: SystemTime, args: &Args) -> FileTimes {
   let file_times = FileTimes::new();
+
   if args.update_access_only {
-    Ok(file_times.set_accessed(time))
+    file_times.set_accessed(time)
   } else if args.update_modification_only {
-    Ok(file_times.set_modified(time))
+    file_times.set_modified(time)
   } else {
-    Ok(file_times.set_accessed(time).set_modified(time))
+    file_times.set_accessed(time).set_modified(time)
   }
 }
