095828c3d6a6255db7c9c2ca54dcd35dbf338afd
Lee Fergusson <lee.fergusson@googlemail.com>
2024-06-11 16:13:35 +0100

    Add basic rcat functionality

--- rcat/src/main.rs
+++ rcat/src/main.rs
@@ -1,3 +1,29 @@
+use std::{fs::File, io::Read};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+/// Concatenate FILE(s) to standard output.
+/// With no FILE, or when FILE is -, read standard input.
+struct Args {
+  #[arg(name = "FILE", required = true)]
+  files: Vec<String>,
+}
+
 fn main() {
-  println!("Hello, world!");
+  let args = Args::parse();
+  
+  for passed_file in args.files {
+    match File::open(&passed_file) {
+      Ok(mut file) => {
+        let mut contents = String::new();
+        match file.read_to_string(&mut contents) {
+            Ok(_) => print!("{}", contents),
+            Err(_) => eprintln!("Could not read file: {}", passed_file),
+        }
+        ;
+      }
+      Err(e) => eprintln!("{}", e),
+    }
+  }
 }
