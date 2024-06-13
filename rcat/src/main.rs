ff7c2758004f08b2c0d9618e0fe00433ee35e440
Lee Fergusson <lee.fergusson@googlemail.com>
2024-06-13 17:06:09 +0100

    Implement line numbering option

--- rcat/src/main.rs
+++ rcat/src/main.rs
@@ -1,29 +1,62 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::{BufRead, BufReader, ErrorKind}, process::exit};
 use clap::Parser;
 
+// CLI ------------------------------------------------------------------------
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 /// Concatenate FILE(s) to standard output.
 /// With no FILE, or when FILE is -, read standard input.
 struct Args {
+  // Options --------------------------------------------------------
+
+  /// Number all output lines.
+  #[arg(short('n'), long("number"))]
+  number: bool,
+
+  // Positional arguments -------------------------------------------
   #[arg(name = "FILE", required = true)]
   files: Vec<String>,
 }
-
+// ----------------------------------------------------------------------------
 fn main() {
   let args = Args::parse();
   
   for passed_file in args.files {
     match File::open(&passed_file) {
-      Ok(mut file) => {
-        let mut contents = String::new();
-        match file.read_to_string(&mut contents) {
-            Ok(_) => print!("{}", contents),
-            Err(_) => eprintln!("Could not read file: {}", passed_file),
+      Ok(file) => {
+        let reader = BufReader::new(file);
+        let lines: Vec<String> = reader.lines().map(|line| {
+          match line {
+            Ok(value) => value,
+            Err(error) => {
+              eprintln!("Error reading file: {}", error);
+              exit(1)
+            }
+          }
+        }).collect();
+
+        for (i, line) in lines.iter().enumerate() {
+          if args.number {
+            println!("{:6} {}", i + 1, line);
+          } else {
+            println!("{}", line);
+          }
         }
-        ;
       }
-      Err(e) => eprintln!("{}", e),
+      Err(error) => match error.kind() {
+        ErrorKind::NotFound => {
+          eprintln!("File not found: {}", passed_file);
+          exit(1)
+        }
+        ErrorKind::PermissionDenied => {
+          eprintln!("Permission denied: {}", passed_file);
+          exit(1)
+        }
+        _ => {
+          eprintln!("Error opening file: {}", passed_file);
+          exit(1)
+        }
+      }
     }
   }
 }
